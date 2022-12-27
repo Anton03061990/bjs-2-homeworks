f08ff6123a93ee8603635491f32537e398e17a62
Anton <anton.solovev.1990@mail.ru>
2022-12-27 17:24:53 +0700

    1 задание

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,20 +1,19 @@
 "use strict"
 
 function solveEquation(a, b, c) {
-  const discr = Math.pow(b, 2) - 4*a*c;
+  const discr = Math.pow(b, 2) - 4 * a * c;
 
-  if (discr < 0)
-  { return []; }
+  if (discr < 0) { return []; }
   if (discr === 0) {
-    let root = -b/(2*a);
+    let root = -b / (2 * a);
     return [root];
   }
   if (discr > 0) {
-    return [(-b + Math.sqrt(discr) )/(2*a), (-b - Math.sqrt(discr) )/(2*a)]
+    return [(-b + Math.sqrt(discr)) / (2 * a), (-b - Math.sqrt(discr)) / (2 * a)]
   }
 }
 
-console.log(solveEquation(-5,-10,5));
+solveEquation(-5, -10, 5);
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
   let payment;
@@ -22,14 +21,16 @@ function calculateTotalMortgage(percent, contribution, amount, countMonths) {
   let monthPer = percent / 100 / 12;
   let bodyCount = amount - contribution;
 
-  if ( isNaN(percent) || isNaN(contribution) || isNaN(amount) || isNaN(countMonths)) {
-    return console.log('Value is not a number');
+  if (isNaN(percent) || isNaN(contribution) || isNaN(amount) || isNaN(countMonths)) {
+    console.log("Value is not a number");
+    return false;
   }
 
   payment = bodyCount * (monthPer + monthPer / ((1 + monthPer) ** countMonths - 1));
+
   sumCount = parseFloat((payment * countMonths).toFixed(2));
-   Math.floor(sumCount * 100) / 100;
-  return sumCount; 
-  
+
+  return sumCount;
 }
-console.log(calculateTotalMortgage(10, 15, 7000, 3));
+
+calculateTotalMortgage(10, 15, 7000, 3);
