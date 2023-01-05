15b80be21af76853715c5698addc5e0bc86e4335
Anton <anton.solovev.1990@mail.ru>
2023-01-05 23:16:44 +0700

    4 задание

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,38 @@
 function Student(name, gender, age) {
-  
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
 }
 
+let Student1 = new Student("Василиса", "женский", 19);
+let Student2 = new Student("Артём", "мужской", 25);
+console.log(Student1);
+console.log(Student2);
+
 Student.prototype.setSubject = function (subjectName) {
-  
+    this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+    if (this.marks !== undefined) {
+        this.marks.push(...marks);
+      }
 }
 
 Student.prototype.getAverage = function () {
-  
+    if (!this.marks || this.marks.length === 0) {
+        return 0;
+      }
+      let sum = 0;
+      for (let i = 0; i < this.marks.length; i++) {
+        sum += this.marks[i];
+      }
+      return sum / this.marks.length;
 }
 
 Student.prototype.exclude = function (reason) {
-  
+    this.excluded = reason;
+    delete this.subject;
+    delete this.marks;
 }
